6f3ca89ca8fa2e55994a1dd397e1c5cd141f96d5
HarshiitaaRajoriiaa <harshitarajoria02@gmail.com>
2025-03-19 21:37:41 +0530

    commit

--- src/Components/Whatapp.jsx
+++ src/Components/Whatapp.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
 
 const Whatapp = () => {
-  const phoneNumber = "919876543210"; // Replace with your WhatsApp number
+  const phoneNumber = "9372158480"; // Replace with your WhatsApp number
 
   return (
     <a
-      href={`https://wa.me/${9372158480}`}
+      href={`https://wa.me/${phoneNumber}`}
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition-transform transform hover:scale-110"
